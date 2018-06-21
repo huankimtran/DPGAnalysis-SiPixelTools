a8d69e2600512dcda95c017f26a8948d151c87a6
theLittleGeek <huankimtran@gmail.com>
2018-06-21 14:26:32 -0500

    fix linking error

--- Library/Pixel/Pixel.h
+++ Library/Pixel/Pixel.h
@@ -6,7 +6,7 @@ public:
 	Pixel(Int_t eventID,Int_t fedID,Int_t layer,Int_t channel,Int_t ROC,Int_t row,Int_t col,Int_t adc);
 	Pixel(){};
 	void fill(Int_t eventID, Int_t fedID,Int_t layer,MyDecode d);
-	void fillEmpty(Int_t eventID, Int_t fedID)
+	void fillEmpty(Int_t eventID, Int_t fedID);
 	
 	Int_t _eventID,_fedID,_layer,_channel,_ROC,_row,_col,_adc;
 };
